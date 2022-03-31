8cfb1acd8c305b3fb259484549411c9874c01ff5
Seokmin Lee <essemfly@gmail.com>
2022-03-31 14:48:39 +0900

    Update for properly setting module and package names

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-cd protos
-
-python -m grpc_tools.protoc --descriptor_set_out=true --include_imports=.. --proto_path=. --python_out=../gen/python --grpc_python_out=../gen/python ./*.proto
-protoc --go_out ../gen/golang --descriptor_set_out=true --include_imports=.. --go_opt paths=source_relative --go-grpc_out ../gen/golang --go-grpc_opt paths=source_relative ./*.proto
+python -m grpc_tools.protoc --descriptor_set_out=true --include_imports=. --proto_path=. --python_out=./gen/python --grpc_python_out=./gen/python ./protos/*.proto
+protoc --go_out ./gen/golang --descriptor_set_out=true --include_imports=. --go_opt paths=source_relative --go-grpc_out ./gen/golang --go-grpc_opt paths=source_relative ./protos/*.proto
