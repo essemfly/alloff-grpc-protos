016d1a4d3e14457124b8aa044ceec7ff4557ab05
Stephen Cheong <j.stephen.cheong@gmail.com>
2022-04-01 22:45:47 +0900

    using python3 for broader workability

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python -m grpc_tools.protoc --descriptor_set_out=true --include_imports=. --proto_path=protos --python_out=./gen/pyalloff --grpc_python_out=./gen/pyalloff ./protos/*.proto
+python3 -m grpc_tools.protoc --descriptor_set_out=true --include_imports=. --proto_path=protos --python_out=./gen/pyalloff --grpc_python_out=./gen/pyalloff ./protos/*.proto
 protoc --go_out ./gen/goalloff --descriptor_set_out=true --include_imports=. --proto_path=protos --go_opt paths=source_relative --go-grpc_out ./gen/goalloff --go-grpc_opt paths=source_relative ./protos/*.proto
 2to3 gen/pyalloff -w -n
