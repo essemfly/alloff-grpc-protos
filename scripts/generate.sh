0121e2066006095d71cc51179cb98f765b4c1acc
Seokmin Lee <essemfly@gmail.com>
2022-04-01 18:06:02 +0900

    Add 2to3 scripts

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -2,3 +2,4 @@
 
 python -m grpc_tools.protoc --descriptor_set_out=true --include_imports=. --proto_path=protos --python_out=./gen/pyalloff --grpc_python_out=./gen/pyalloff ./protos/*.proto
 protoc --go_out ./gen/goalloff --descriptor_set_out=true --include_imports=. --proto_path=protos --go_opt paths=source_relative --go-grpc_out ./gen/goalloff --go-grpc_opt paths=source_relative ./protos/*.proto
+2to3 gen/pyalloff -w -n
